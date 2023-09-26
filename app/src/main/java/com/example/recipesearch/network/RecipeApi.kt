a33113051db5d158d748630d3c5141d822dce8b2
shhilja <71275736+shhilja@users.noreply.github.com>
2023-09-26 20:42:56 +0200

    Update RecipeApi.kt

--- app/src/main/java/com/example/recipesearch/network/RecipeApi.kt
+++ app/src/main/java/com/example/recipesearch/network/RecipeApi.kt
@@ -5,16 +5,15 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-//GET: recipes/v2
-//appKey&ID: ?type=public&app_id=5bc59021&app_key=a25ecb8b5f75d554a674c8db2b857c39"
+
 
 interface RecipeApi {
     @GET("recipes/v2")
     fun getRecipeHits(
         @Query("type") type:String = "public",
         @Query("q") q : String = "salmon",
-        @Query("app_id") app_id : String = "5bc59021",
-        @Query("app_key") app_key : String = "a25ecb8b5f75d554a674c8db2b857c39"
+        @Query("app_id") app_id : String = "",
+        @Query("app_key") app_key : String = ""
 
     ): Call<RecipeHits>
 
@@ -22,8 +21,8 @@ interface RecipeApi {
     fun getRecipeByTime(
         @Query("mealType") mealType: String = "Breakfast",
         @Query("type") type:String = "public",
-        @Query("app_id") app_id : String = "5bc59021",
-        @Query("app_key") app_key : String = "a25ecb8b5f75d554a674c8db2b857c39"
+        @Query("app_id") app_id : String = "",
+        @Query("app_key") app_key : String = ""
 
     ): Call<RecipeHits>
 
